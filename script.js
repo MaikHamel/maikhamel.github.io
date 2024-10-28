678b1fc29539deabdc4493f59510062380b6b805
maikhamel <hamm3157@gmail.com>
2024-10-27 22:55:09 -0400

    ajout

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 
+
+
 const handleOnMouseMove = e => {
     const { currentTarget: target} = e;
 
@@ -34,48 +36,12 @@ const swiper = new Swiper('.carousel-container', {
   
     // And if we need scrollbar
     
-  });
+});
 
 
-  const backgroundgravity = document.querySelector("#backgroundgravity");
 
-  let Engine = Matter.Engine,
-          Render = Matter.Render,
-          Runner = Matter.Runner,
-          Bodies = Matter.Bodies,
-          Composite = Matter.Composite;
 
-      // create an engine
-      let engine = Engine.create();
 
-      // create a renderer
-      let render = Render.create({
-          element: backgroundgravity,
-          engine: engine,
-          options:{
-            width:backgroundgravity.clientWidth,
-            height:backgroundgravity.clientHeight,
-            background: "transparent",
-            
-          }
-      });
-
-      // create two boxes and a ground
-      let boxA = Bodies.rectangle(400, 200, 80, 80);
-      let boxB = Bodies.rectangle(450, 50, 80, 80);
-      let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
-      // add all of the bodies to the world
-      Composite.add(engine.world, [boxA, boxB, ground]);
-
-      // run the renderer
-      Render.run(render);
-
-      // create runner
-      let runner = Runner.create();
-
-      // run the engine
-      Runner.run(runner, engine);
 
 
 
